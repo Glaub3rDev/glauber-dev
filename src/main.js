307f5f4f2e9aa2b9168cb011795b62ab04d41fe0
Glauber Martins <mglauber071@gmail.com>
2026-06-22 22:58:47 -0300

    .
    .

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-import { navbar } from "./sections/navbar.js";
-import { hero } from "./sections/hero.js";
-
-document.querySelector("#app").innerHTML = `
-  ${navbar()}
-  ${hero()}
-`;
